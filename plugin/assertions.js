c569623bc44b6de43aea56aad41ab95b5dd6f488
Johnny Estilles <johnny.estilles@agentia.asia>
2015-05-24 12:40:54 +0800

    remove docs/comments for now, will document later

--- plugin/assertions.js
+++ plugin/assertions.js
@@ -2,14 +2,6 @@
 
 var kindof = require('kindof');
 
-/**
- * Assert that an object is a promise.
- *
- * @example
- * (new Promise(function() {})).must.have.length(18)
- *
- * @method promise
- */
 exports.promise = function() {
   var isAnObject = kindof(this.actual) === 'object';
   var isThenable = this.isEnumerable(this.actual, 'then') && kindof(this.actual.then) === 'function';
